687b3b5d965ad1d04e0587747030a96e6385fec7
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-06-29 19:03:03 +0530

    Add if-else examples

---
+++ 02_Conditional_Statements/if_else.py
@@ -0,0 +1,17 @@
+score = 78
+attendance = 84
+
+if score >= 40 and attendance >= 75:
+    print("Student passed the eligibility criteria.")
+else:
+    print("Student did not meet the eligibility criteria.")
+
+
+temperature = 29
+
+if temperature >= 35:
+    print("Hot")
+elif temperature >= 25:
+    print("Warm")
+else:
+    print("Cool")
