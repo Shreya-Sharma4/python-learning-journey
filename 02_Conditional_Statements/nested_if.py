83f52c3471429dd9a49799d538f9df20d8a2cede
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-06-30 22:12:19 +0530

    Add nested if examples

---
+++ 02_Conditional_Statements/nested_if.py
@@ -0,0 +1,10 @@
+age = 22
+has_license = True
+
+if age >= 18:
+    if has_license:
+        print("Eligible to drive.")
+    else:
+        print("Driving license is required.")
+else:
+    print("Not eligible to drive.")
