698a7e704d128c01a51c1299ef628e7540e669c8
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-13 11:09:28 +0530

    Add string methods examples

---
+++ 07_Strings/string_methods.py
@@ -0,0 +1,10 @@
+text = "Python Programming"
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.capitalize())
+print(text.swapcase())
+print(text.replace("Programming", "Developer"))
+print(text.count("m"))
+print(text.find("thon"))
