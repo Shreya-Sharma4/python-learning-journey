c68620b65a5db7113a7a12d60e2862abc2fb2e99
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-16 22:46:27 +0530

    Add ASCII operations examples

---
+++ 07_Strings/ascii_operations.py
@@ -0,0 +1,9 @@
+character = "A"
+
+print(f"Character : {character}")
+print(f"ASCII     : {ord(character)}")
+
+ascii_value = 97
+
+print(f"ASCII     : {ascii_value}")
+print(f"Character : {chr(ascii_value)}")
