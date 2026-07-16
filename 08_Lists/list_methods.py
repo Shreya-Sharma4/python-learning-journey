cad0c8784a060861384264c534a5ff2aecac8a1f
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-16 22:47:48 +0530

    Add list methods examples

---
+++ 08_Lists/list_methods.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana", "Orange"]
+
+fruits.append("Mango")
+fruits.insert(1, "Grapes")
+fruits.remove("Banana")
+fruits.sort()
+
+print(fruits)
+print(f"Length: {len(fruits)}")
