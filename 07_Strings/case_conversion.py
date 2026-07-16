4c01528885e1d05c770c833be467c341d8f8cb1b
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-16 22:45:55 +0530

    Add case conversion examples

---
+++ 07_Strings/case_conversion.py
@@ -0,0 +1,8 @@
+text = "pYtHoN ProGRamMing"
+
+print(f"Original   : {text}")
+print(f"Upper      : {text.upper()}")
+print(f"Lower      : {text.lower()}")
+print(f"Title      : {text.title()}")
+print(f"Capitalize : {text.capitalize()}")
+print(f"Swapcase   : {text.swapcase()}")
