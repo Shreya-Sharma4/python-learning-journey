0d2eaaa0940a29e0b0c719fc2ed96e84b1ee6770
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-19 23:20:23 +0530

    Add tuple basics examples

---
+++ 09_Tuples/tuple_basics.py
@@ -0,0 +1,8 @@
+fruits = ("Apple", "Banana", "Orange", "Mango")
+
+print(f"First Item: {fruits[0]}")
+print(f"Last Item: {fruits[-1]}")
+print(f"Total Items: {len(fruits)}")
+
+for fruit in fruits:
+    print(fruit)
