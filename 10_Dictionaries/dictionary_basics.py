82e74952686ddb08c45476d30d60b75592fdd498
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-20 09:32:33 +0530

    Add dictionary basics examples

---
+++ 10_Dictionaries/dictionary_basics.py
@@ -0,0 +1,15 @@
+student = {
+    "name": "Shruti",
+    "age": 20,
+    "course": "CSE"
+}
+
+print(student)
+
+print(f"Name: {student['name']}")
+print(f"Age: {student['age']}")
+
+student["age"] = 21
+student["city"] = "Amravati"
+
+print(student)
