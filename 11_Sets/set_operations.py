533bd8e9a28662d958a19d72a48dde28e9bf6853
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-21 19:34:50 +0530

    Add set operations examples

---
+++ 11_Sets/set_operations.py
@@ -0,0 +1,7 @@
+first_set = {10, 20, 30, 40}
+second_set = {30, 40, 50, 60}
+
+print(f"Union: {first_set | second_set}")
+print(f"Intersection: {first_set & second_set}")
+print(f"Difference: {first_set - second_set}")
+print(f"Symmetric Difference: {first_set ^ second_set}")
