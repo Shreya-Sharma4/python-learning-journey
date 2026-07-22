a1d2b6c1cefa909eaabd3862c274a9df15609a68
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-22 09:20:11 +0530

    Add math module examples

---
+++ 12_Modules/math_module.py
@@ -0,0 +1,9 @@
+import math
+
+number = 25
+
+print(f"Square Root: {math.sqrt(number)}")
+print(f"Power: {math.pow(5, 3)}")
+print(f"Ceil: {math.ceil(7.2)}")
+print(f"Floor: {math.floor(7.8)}")
+print(f"Factorial: {math.factorial(5)}")
