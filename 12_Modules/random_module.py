4c378d55119941912f60d369f4a11432b9df3aa0
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-22 09:22:30 +0530

    Add random module examples

--- 12_Modules/random_module.py
+++ 12_Modules/random_module.py
@@ -2,8 +2,12 @@ import random
 
 numbers = [10, 20, 30, 40, 50]
 
-print(f"Random Number: {random.randint(1, 100)}")
-print(f"Random Choice: {random.choice(numbers)}")
+print(f"Random Integer      : {random.randint(1, 100)}")
+print(f"Random Float        : {random.uniform(1, 10)}")
+print(f"Random Choice       : {random.choice(numbers)}")
+print(f"Random Sample       : {random.sample(numbers, 3)}")
 
 random.shuffle(numbers)
-print(f"Shuffled List: {numbers}")
+print(f"Shuffled List       : {numbers}")
+
+print(f"Random Range Value  : {random.randrange(10, 51, 5)}")
