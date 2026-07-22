3db3c2f08fab3a2eb3b1d1f641f5964abd11778e
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-07-22 09:25:01 +0530

    Add math module examples

--- 12_Modules/math_module.py
+++ 12_Modules/math_module.py
@@ -1,9 +1,14 @@
 import math
 
 number = 25
+radius = 2
 
-print(f"Square Root: {math.sqrt(number)}")
-print(f"Power: {math.pow(5, 3)}")
-print(f"Ceil: {math.ceil(7.2)}")
-print(f"Floor: {math.floor(7.8)}")
-print(f"Factorial: {math.factorial(5)}")
+print(f"Square Root : {math.sqrt(number)}")
+print(f"Factorial   : {math.factorial(5)}")
+print(f"Ceil        : {math.ceil(45.20)}")
+print(f"Floor       : {math.floor(45.99)}")
+print(f"Power       : {math.pow(2, 3)}")
+print(f"Pi          : {math.pi}")
+
+circumference = 2 * math.pi * radius
+print(f"Circumference: {circumference:.2f}")
