f12baf67a47f4ffea260542fd358c3cf7c8aaf7f
Shreya-Sharma4 <sharmashreya13794@gmail.com>
2026-08-16 09:16:01 +0530

    Added hierarchical inheritance person

---
+++ 15_Object_Oriented_Programming/Hierarchical Inheritance/person.py
@@ -0,0 +1,8 @@
+class person:
+    def __init__(self, name, id, skill):
+        self.name = name
+        self.id = id
+        self.skill = skill
+
+    def bonus(self):
+        print("Your bonus is ...")
